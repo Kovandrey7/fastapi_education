4bd2994f552175cf311482d6cdeaeb6b1089dec1
Андрей <kov.andrey7@yandex.ru>
2024-01-07 15:14:43 +0300

    Initial commit

---
+++ database/db_helper.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from config import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
+        )
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+
+db_helper = DatabaseHelper(url=settings.DB_URL, echo=settings.echo)
