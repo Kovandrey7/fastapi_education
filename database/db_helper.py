2a72b7b02f33c49a89f69e8838876f1bcb3cb21e
Андрей <kov.andrey7@yandex.ru>
2024-01-30 21:59:04 +0300

    Deleted session dependency

--- database/db_helper.py
+++ database/db_helper.py
@@ -14,10 +14,5 @@ class DatabaseHelper:
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
 
-    async def session_dependency(self) -> AsyncSession:
-        async with self.session_factory() as session:
-            yield session
-            await session.close()
-
 
 db_helper = DatabaseHelper(url=settings.DB_URL, echo=settings.ECHO)
